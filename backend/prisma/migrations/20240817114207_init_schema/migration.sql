e860c5e614ae3e5cb436499eb2bf87e30069beb2
Contact app <harrsh100045@gmail.com>
2024-08-17 11:52:13 +0530

    store changes

---
+++ backend/prisma/migrations/20240817114207_init_schema/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Theatre" ADD COLUMN     "Address" TEXT,
+ADD COLUMN     "city" TEXT,
+ADD COLUMN     "image" TEXT,
+ADD COLUMN     "seats" INTEGER,
+ADD COLUMN     "ticketPrice" DOUBLE PRECISION;
