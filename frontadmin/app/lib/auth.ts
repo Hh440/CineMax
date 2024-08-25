cfa665d0c29a54f5d76e085bf950dca6fce9fd40
Aasif <aa6125405@gmail.com>
2024-08-25 13:03:41 +0530

    added app/lib/auth.ts

---
+++ frontadmin/app/lib/auth.ts
@@ -0,0 +1,40 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (credentials.username === "admin" && credentials.password === "signin") {
+          return { id: 1, name: "Admin", username: "admin" };
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/auth/signin',
+    error: '/auth/error',
+  },
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.id = token.id;
+      return session;
+    },
+  },
+};
