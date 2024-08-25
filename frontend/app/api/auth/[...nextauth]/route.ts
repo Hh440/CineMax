6622bbc77f78c998e4dccbd4fa68e8af974f6191
Hh440 <harsh100045@gmail.com>
2024-08-25 17:25:05 +0530

    auhth

--- frontend/app/api/auth/[...nextauth]/route.ts
+++ frontend/app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google"
 
 
-const handler =NextAuth({
+const authOptions :AuthOptions={
     providers:[
         GoogleProvider({
             clientId:process.env.GOOGLE_CLIENT_ID   as string,
@@ -47,7 +47,9 @@ const handler =NextAuth({
         strategy:'jwt'
     },
     secret:process.env.NEXTAUTH_SECRET
-})
+}
+
+const handler = NextAuth(authOptions)
 
 
 export {handler as GET ,handler as POST}
